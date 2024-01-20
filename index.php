5a71f52cac8c355d02a63df449b680fdc9aa0120
NoWayItsJJ <fabio.pauletta@gmail.com>
2024-01-20 12:01:19 +0100

    minor changes

--- index.php
+++ index.php
@@ -48,12 +48,12 @@ for ($i = 0; $i < 5; $i++) {
             <div class="up-container">
                 <div class="mazzo">
                     <div class="titlemazzo"><h1>MAZZO</h1></div>
-                    <div class="cartamazzo"><img src="./carte/back.gif" alt="" /></div>
+                    <div class="cartamazzo"><img src="./carte/back.gif" /></div>
                 </div>
                 <div class="tavolo">
                     <div class="cartetavolo">
                         <?php foreach ($carteTavolo as $carta) : ?>
-                            <div class="carta"><img src="./carte/<?php echo $carta; ?>" alt="" /></div>
+                            <div class="carta"><img src="./carte/<?php echo $carta; ?>"/></div>
                         <?php endforeach; ?>
                     </div>
                 </div>
@@ -62,29 +62,29 @@ for ($i = 0; $i < 5; $i++) {
                 <div class="giocatore">
                     <div class="nomegiocatore"><h1>PLAYER 1</h1></div>
                     <div class="carte">
-                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore1[0]; ?>" alt="" /></div>
-                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore1[1]; ?>" alt="" /></div>
+                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore1[0]; ?>" /></div>
+                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore1[1]; ?>" /></div>
                     </div>
                 </div>
                 <div class="giocatore">
                     <div class="nomegiocatore"><h1>PLAYER 2</h1></div>
                     <div class="carte">
-                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore2[0]; ?>" alt="" /></div>
-                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore2[1]; ?>" alt="" /></div>
+                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore2[0]; ?>" /></div>
+                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore2[1]; ?>" /></div>
                     </div>
                 </div>
                 <div class="giocatore">
                     <div class="nomegiocatore"><h1>PLAYER 3</h1></div>
                     <div class="carte">
-                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore3[0]; ?>" alt="" /></div>
-                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore3[1]; ?>" alt="" /></div>
+                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore3[0]; ?>" /></div>
+                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore3[1]; ?>" /></div>
                     </div>
                 </div>
                 <div class="giocatore">
                     <div class="nomegiocatore"><h1>PLAYER 4</h1></div>
                     <div class="carte">
-                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore4[0]; ?>" alt="" /></div>
-                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore4[1]; ?>" alt="" /></div>
+                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore4[0]; ?>" /></div>
+                        <div class="carta"><img src="./carte/<?php echo $carteGiocatore4[1]; ?>" /></div>
                     </div>
                 </div>
             </div>
